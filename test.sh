2d46b7945a1c5ec2135bcbc586a36e3adb7b4de8
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-09-05 10:24:40 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,7 @@
 # Test the output of our "Hello" script
 output=$(./hello.sh)
 
-if [ $output != "Hello" ]
-	then
+if [ $output != "Hello" ]; then
 	echo "Expecting: 'Hello', received: '$output'"
 	exit 1
 fi
@@ -12,4 +11,12 @@ fi
 echo "'Hello' script is good"
 
 #check if the nginx has been sucessfully installed
-which nginx && echo "nginx IS installed" || (echo "nginx is NOT installed" && exit 1)
+which nginx 
+if [ $? -eq 0 ]; then
+  echo "nginx IS installed"
+else
+  echo "nginx is NOT installed"
+  exit 1
+fi
+
+echo "test completed"
